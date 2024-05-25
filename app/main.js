33906d92186419e8ff6a70c37e12f73498fb474a
Blaze1012 <atif251171@gmail.com>
2024-05-26 01:10:19 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -14,7 +14,7 @@ const server = net.createServer((connection) => {
     if (commands[2] == "ECHO") {
       const str = commands[4];
       const l = str.length;
-      connection.write(str);
+      connection.write("$" + l + "\r\n" + str + "\r\n");
     }
   });
 });
