1c760005c4659e171f8852cf09db7a8ba458928b
Blaze1012 <atif251171@gmail.com>
2024-05-26 00:33:37 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -8,7 +8,6 @@ const server = net.createServer((connection) => {
   // Handle connection
 
   connection.write("+PONG\r\n");
-  connection.write("+PONG\r\n");
 });
 
 server.listen(6379, "127.0.0.1");
