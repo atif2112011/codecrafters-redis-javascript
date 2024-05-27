ae8843fb078bdf16faa837f7faf4315c37a258e3
Blaze1012 <atif251171@gmail.com>
2024-05-27 23:29:19 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -180,7 +180,7 @@ const server = net.createServer((connection) => {
           delete db[key];
         }, commands[10]);
 
-      propagateToReplicas(commands);
+      propagateToReplicas(data);
       connection.write("+OK\r\n");
     } else if (commands[2] == "GET") {
       const answer = db[commands[4]];
